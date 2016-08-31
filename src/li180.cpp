5fa8f349b08e52d7e1083816f0fb514c2e0c4a3f
Rafael Henrique Tibaes <r@fael.nl>
2016-08-31 15:58:08 -0300

    leopard streaming

---
+++ src/li180.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+#include <stdexcept>
+
+const int cameraID = 0;
+const int cameraHeight = 3684;
+const int cameraWidth = 4912;
+const int cameraFPS = 4;
+
+int main(int argc, char **argv) {
+  cv::VideoCapture cap;
+  cap.open(cameraID);
+  if (!cap.isOpened())
+    throw std::runtime_error("Could not open selected device");
+
+  videoCapture.set(cv::CAP_PROP_FRAME_WIDTH, cameraWidth);
+  videoCapture.set(cv::CAP_PROP_FRAME_HEIGHT, cameraHeight);
+  videoCapture.set(cv::CAP_PROP_FPS, cameraFPS);
+  videoCapture.set(cv::CAP_PROP_CONVERT_RGB, 0);
+
+  bool capturing = true;
+  int frameWrote = 0;
+  cv::Mat frame;
+  while (capturing) {
+    cap >> frame;
+    cv::Mat raw(cameraHeight, cameraWidth, CV_16UC1, frame.data);
+    if (raw.empty()) {
+      std::cout << "Captured an empty image." << std::endl;
+      continue;
+    }
+
+    cv::Mat bayer8BitMat = raw.clone();
+    // Convert the Bayer data from 16-bit to to 8-bit
+    // The 3rd parameter here scales the data by 1/16 so that it fits in 8 bits.
+    // Without it, convertTo() just seems to chop off the high order bits.
+    bayer8BitMat.convertTo(bayer8BitMat, CV_8UC1, 0.0625);
+
+    // Convert the Bayer data to 8-bit RGB
+    cv::Mat rgb8BitMat(cameraHeight, cameraWidth, CV_8UC3);
+    cv::cvtColor(bayer8BitMat, rgb8BitMat, CV_BayerGB2RGB);
+
+    cv::imshow("Frame", rgb8BitMat);
+    char cmd = cv::waitKey(10);
+
+    if (cmd == 'c') {
+      std::ostringstream s_path;
+      s_path << "leopard_sample_" << frameWrote++ << ".jpg";
+      cv::imwrite(s_path.str(), frame);
+    } else if (cmd == 'q')
+      capturing = false;
+  }
+
+  return 0;
+}
